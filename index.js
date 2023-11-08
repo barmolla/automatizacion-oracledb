ace01e579e80e8e2e2aa7943ea623e2cef6a185d
Brian Armolla <barmolla@redhat.com>
2023-11-08 16:14:41 -0300

    WIP Dockerfile y shell script

--- index.js
+++ index.js
@@ -9,53 +9,68 @@ const TIMESTAMP = 'TIMESTAMP';
 const NUMBER = 'NUMBER';
 const VARCHAR2 = 'VARCHAR2';
 
+let contadorPromesa = '00';
+
 function generarInserts(archivo, nombreTabla) {
-    const inserts = [];
+    return new Promise((resolve, reject) => {
+        const inserts = [];
 
-    fs.createReadStream(archivo)
-        .pipe(csv())
-        .on('data', (row) => {
-            const values = Object.values(row).map(value => `'${value}'`).join(', ');
-            const tablaEncontrada = tablas.find(tabla => tabla['nombreTabla'] == nombreTabla);
-            const campos = Object.keys(row);
-            const valores = Object.values(row);
-            const campoTipoValor = campos.map((campo, indice) => {
-                const definicionCampo = tablaEncontrada['lineasCreate'].find(linea => linea.includes(campo));
-                const tipo = definicionCampo.includes(TIMESTAMP) ? TIMESTAMP :
-                             definicionCampo.includes(VARCHAR2) ? VARCHAR2 :
-                             NUMBER;
-                const valor = valores[indice]
+        fs.createReadStream(archivo)
+            .pipe(csv())
+            .on('data', (row) => {
+                const values = Object.values(row).map(value => `'${value}'`).join(', ');
+                const tablaEncontrada = tablas.find(tabla => tabla['nombreTabla'] === nombreTabla);
+                const campos = Object.keys(row);
+                const valores = Object.values(row);
+                const campoTipoValor = campos.map((campo, indice) => {
+                    const definicionCampo = tablaEncontrada['lineasCreate'].find(linea => linea.includes(campo));
+                    const tipo = definicionCampo.includes(TIMESTAMP) ? TIMESTAMP :
+                                 definicionCampo.includes(VARCHAR2) ? VARCHAR2 :
+                                 NUMBER;
+                    const valor = valores[indice];
 
-                return {
-                    campo,
-                    tipo,
-                    valor
-                }
-            })
+                    return {
+                        campo,
+                        tipo,
+                        valor
+                    };
+                });
 
-            const query = `INSERT INTO ${nombreTabla} (${campos.join(', ')}) VALUES (${campoTipoValor.map(({ tipo, valor }) => {
-                    if (tipo == NUMBER) {
-                        return valor && valor != '' ? valor : 'NULL';
-
-                    } else if (tipo == TIMESTAMP) {
-                        return valor && valor != '' ? `TIMESTAMP \'${valor}\'` : 'NULL';
-
+                const query = `INSERT INTO ${nombreTabla} (${campos.join(', ')}) VALUES (${campoTipoValor.map(({ tipo, valor }) => {
+                    if (tipo === NUMBER) {
+                        return valor && valor !== '' ? valor : 'NULL';
+                    } else if (tipo === TIMESTAMP) {
+                        return valor && valor !== '' ? `TIMESTAMP '${valor}'` : 'NULL';
                     } else {
-                        return valor && valor != '' ? `\'${valor}\'` : 'NULL';
+                        return valor && valor !== '' ? `'${valor}'` : 'NULL';
                     }
-                } )});`;
+                }).join(', ')});`;
 
-            inserts.push(query);
-        })
-        .on('end', () => {
-            fs.writeFile(`${carpetaScripts}${nombreTabla}.sql`, inserts.join('\n'), (err) => {
-                if (err) throw err;
-                console.log(`SQL generado para ${nombreTabla}`);
+                inserts.push(query);
+            })
+            .on('end', () => {
+                const contadorPromesaAsNumber = parseInt(contadorPromesa) + 1;
+
+                contadorPromesa = contadorPromesaAsNumber < 10 ? `0${contadorPromesaAsNumber}`: `${contadorPromesaAsNumber}`;
+            
+                fs.writeFile(`${carpetaScripts}${contadorPromesa} - ${nombreTabla}.sql`, inserts.join('\n'), (err) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        console.log(`SQL generado para ${nombreTabla}`);
+                        
+                        resolve();
+                    }
+                });
+            })
+            .on('error', (err) => {
+                reject(err);
             });
-        });
+    });
 }
 
-fs.readdir(carpeta, (err, archivos) => {
+fs.mkdirSync('./scripts/', { recursive: true });
+fs.readdir(carpeta, async (err, archivos) => {
     if (err) {
         return console.error('Error al leer la carpeta', err);
     }
@@ -73,13 +88,19 @@ fs.readdir(carpeta, (err, archivos) => {
 
     armarMapaTablaColumnas();
 
-    tablasAProcesar.forEach(({ rutaArchivo, nombreTabla }) => generarInserts(rutaArchivo, nombreTabla));
+    tablasAProcesar.forEach(({ rutaArchivo, nombreTabla }) => 
+        generarInserts(rutaArchivo, nombreTabla)
+    );
+
+    const promises = tablasAProcesar.map(({ rutaArchivo, nombreTabla }) => generarInserts(rutaArchivo, nombreTabla));
+
+    await Promise.all(promises);
 
     generarDockerfile();
     generarShellScript();
 
     const used = process.memoryUsage().heapUsed / 1024 / 1024;
-    
+
     console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
 });
 
@@ -87,9 +108,11 @@ function generarShellScript() {
     const lineasShellScript = [];
 
     lineasShellScript.push("#!/bin/bash");
-    lineasShellScript.push("docker build . -t oracle:latest");
+    lineasShellScript.push("");
     lineasShellScript.push("docker rm -f oracle");
+    //lineasShellScript.push("docker image rm -f oracle");
     lineasShellScript.push("docker volume rm oracle-volume");
+    lineasShellScript.push("docker build . -t oracle:latest");
     lineasShellScript.push("docker run -d -p 1521:1521 -v oracle-volume:/opt/oracle/oradata -e ORACLE_PASSWORD=oracle --name oracle oracle");
 
     fs.writeFile(`${carpetaScripts}docker-run.sh`, lineasShellScript.join('\n'), (err) => {
@@ -104,9 +127,12 @@ function generarDockerfile() {
 
     lineasDockerfile.push(`FROM ${imagenBase}`);
     lineasDockerfile.push('ARG dst="/container-entrypoint-initdb.d/"');
+    lineasDockerfile.push('ARG src="00 - ddl-gestor.sql"');
+    lineasDockerfile.push('ADD $\{src\} $\{dst\}');
 
     fs.readdir(carpetaScripts, (err, archivosGenerados) => {
-        const instruccionesAdd = archivosGenerados.map(archivo => `ADD ${archivo} $\{dst\}`);
+        //lineasDockerfile.push(`ARG src="${archivo}"`);
+        const instruccionesAdd = archivosGenerados.map(archivo => `ARG src="${archivo}"\nADD $\{src\} $\{dst\}`);
 
         lineasDockerfile.push(...instruccionesAdd);
 
